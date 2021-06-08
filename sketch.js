4a570191976648e22c4a436e46fa6778d0ba3288
Subhra Pattnaik <50340351+subhrapattnaik@users.noreply.github.com>
2021-06-08 09:32:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var n1,n2;
+var b1,b2,b3,b4;
+
+function setup() {
+  
+  createCanvas(400, 400);
+  
+  num1 = createInput();
+  num1.position(5, 60);
+  
+  num2 = createInput();
+  num2.position(200, 60);
+
+ 
+
+
+  b1 = createButton("ADD");
+  b1.position(10, 200);
+  b1.mousePressed(add);
+  
+  b2 = createButton("Multiply");
+  b2.position(100, 200);
+  b2.mousePressed(mul);
+  
+  
+  textAlign(CENTER);
+  textSize(15);
+}
+
+function draw() {
+  background(200,10,200)
+  text("Number 1",70,50)
+  text("Number 2",270,50)
+  
+
+}
+
+function add(){
+  n1=parseInt(num1.value())
+  n2=parseInt(num2.value())
+var sum=n1+n2
+console.log(sum)
+var displaySum=createElement('h2');
+displaySum.html("Sum of Numbers :"+sum)
+displaySum.position(10,300);
+}
+
+function mul(){
+  n1=parseInt(num1.value())
+  n2=parseInt(num2.value())
+  var product=n1*n2
+  console.log(product)
+  var displayProduct=createElement('h2');
+  displayProduct.html("Product of Numbers :"+product)
+  displayProduct.position(10,400);
+  }
